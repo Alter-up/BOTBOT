6f3042333f404e79aa46f441725e6fc486a02d3a
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-26 12:33:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,115 +1,112 @@
-const { Client, Intents } = require("discord.js");
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// const { clientId, guildId, token, publicKey } = require('./config.json');
+require('dotenv').config()
+const APPLICATION_ID = process.env.APPLICATION_ID 
+const TOKEN = process.env.TOKEN 
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
+const GUILD_ID = process.env.GUILD_ID 
 
-client.on("message", (msg) => {
-  let isWelcomeMessage = msg.type === "GUILD_MEMBER_JOIN";
 
-  if (isWelcomeMessage) {
-    msg.author.send({"content": `Hey <@${msg.author.id}> it's time to check your **"Rewards"**!` ,
-   
-    "embeds": [
-        {
-            "title": "Check community rewards",
-            "description": "This server is send you a atomatical message. Please verify yourself to get access interact. [Click here](https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join) to get started!",
-            "color": 39129,
-            "timestamp": "2024-02-20T18:58:17.660Z",
-            "url": "https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join",
-            "author": {
-                "name": "MEE6",
-                "url": "https://discord.com",
-                "icon_url": "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=160"
-            },
-            "thumbnail": {
-                "url": "https://cdn.discordapp.com/icons/1208721793532039209/68cc2065a3e1f8218f8a9e3b35fa8294.png?size=240"
-            },
-            "image": {
-                "url": ""
-            },
-            "footer": {
-                "text": ""
-            }
-        }
-    ],
-    "components": [
-        {
-            "type": 1,
-            "components": [
-                {
-                    "type": 2,
-                    "style": 5,
-                    "label": "CHECK YOUR REWARDS",
-                    "url": "https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join",
-                    "disabled": false
-                }
-            ]
-        }
-    ],
-});
+const axios = require('axios')
+const express = require('express');
+const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
-    client.channels.cache
-      .get("1210888935899660340")
-      .send({"content": `Hey <@${msg.author.id}> it's time to check your **"Rewards"**!\n\n` ,
-     
-      "embeds": [
-          {
-              "title": "Check community rewards",
-              "description": "This server is send you a atomatical message. Please verify yourself to get access interact. [Click here](https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join) to get started!",
-              "color": 39129,
-              "timestamp": "2024-02-20T18:58:17.660Z",
-              "url": "https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join",
-              "author": {
-                  "name": "MEE6",
-                  "url": "https://discord.com",
-                  "icon_url": "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=160"
-              },
-              "thumbnail": {
-                  "url": "https://cdn.discordapp.com/icons/1208721793532039209/68cc2065a3e1f8218f8a9e3b35fa8294.png?size=240"
-              },
-              "image": {
-                  "url": ""
-              },
-              "footer": {
-                  "text": ""
-              }
-          }
-      ],
-      "components": [
-          {
-              "type": 1,
-              "components": [
-                  {
-                      "type": 2,
-                      "style": 5,
-                      "label": "CHECK REWARDS",
-                      "url": "https://discord.com/api/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.check2.app%2Fdashboard&scope=identify+guilds+guilds.join",
-                      "disabled": false
-                  }
-              ]
-          }
-      ],
-  });
-  
-	
-      
+
+const app = express();
+// app.use(bodyParser.json());
+
+const discord_api = axios.create({
+  baseURL: 'https://discord.com/api/',
+  timeout: 3000,
+  headers: {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+	"Access-Control-Allow-Headers": "Authorization",
+	"Authorization": `Bot ${TOKEN}`
   }
-  
-
-  let prefix = "!";
-  let message = msg.content;
-
-  let channel = msg.channelId;
-  let botChannel = "1210888935899660340";
-
-  const sendMessage = (message) => {
-    client.channels.cache.get(botChannel).send(message);
-  };
 });
 
-client.login("MTIxMDg4NzI0NTY4NzU1ODIzNA.GQNS3G.eHr4LGLfu5KuWvXM899ikz3mYlz6zOAlch33oA");
+
+
+
+app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
+  const interaction = req.body;
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    console.log(interaction.data.name)
+    if(interaction.data.name == 'yo'){
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Yo ${interaction.member.user.username}!`,
+        },
+      });
+    }
+
+    if(interaction.data.name == 'dm'){
+      // https://discord.com/developers/docs/resources/user#create-dm
+      let c = (await discord_api.post(`/users/@me/channels`,{
+        recipient_id: interaction.member.user.id
+      })).data
+      try{
+        // https://discord.com/developers/docs/resources/channel#create-message
+        let res = await discord_api.post(`/channels/${c.id}/messages`,{
+          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+        })
+        console.log(res.data)
+      }catch(e){
+        console.log(e)
+      }
+
+      return res.send({
+        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data:{
+          content:'👍'
+        }
+      });
+    }
+  }
+
+});
+
+
+
+app.get('/register_commands', async (req,res) =>{
+  let slash_commands = [
+    {
+      "name": "yo",
+      "description": "replies with Yo!",
+      "options": []
+    },
+    {
+      "name": "dm",
+      "description": "sends user a DM",
+      "options": []
+    }
+  ]
+  try
+  {
+    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
+    let discord_response = await discord_api.put(
+      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+      slash_commands
+    )
+    console.log(discord_response.data)
+    return res.send('commands have been registered')
+  }catch(e){
+    console.error(e.code)
+    console.error(e.response?.data)
+    return res.send(`${e.code} error from discord`)
+  }
+})
+
+
+app.get('/', async (req,res) =>{
+  return res.send('Follow documentation ')
+})
+
+
+app.listen(8999, () => {
+
+})
